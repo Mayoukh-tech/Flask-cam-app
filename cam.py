173e27edc4c924f47ebcb20de30ec673672afec7
techyfreek <114090817+Mayoukh-tech@users.noreply.github.com>
2023-10-07 16:06:22 +0530

    cam.py

--- cam.py
+++ cam.py
@@ -32,37 +32,6 @@ def vid_ft():
 if __name__ == "__main__":
     app.run(debug=True ,port=8523)
 
-
-# *************************************************************#
-    # @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
-
-# def learn():
-    # Import essential libraries  
-# Replace the below URL with your own. Make sure to add "/shot.jpg" at last.
-# url = "http://192.168.0.102:8080/shot.jpg"
-  
-# # While loop to continuously fetching data from the Url
-# while True:
-#     img_resp = requests.get(url)
-#     img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
-#     img = cv2.imdecode(img_arr, -1)
-#     img = imutils.resize(img, width=1000, height=1800)
-#     cv2.imshow(" ",img)
-
-#         # Press Esc key to exit
-#     if cv2.waitKey(1) == 27:
-#         break
-   
-#     cv2.destroyAllWindows()
-# port can be changed by below line
-# app.run(debug=True, port=1000) instead of 1000 any number can be used
-# by default port will be choosen by flask python itself so to change it you must use above line of code
-
-# here we have to make 2 files mainly templates and static and both should be in the same path where flask app is made
-# http://127.0.0.1:5000/static/m.txt
 '''running the above lines in the search bar 
 will reflect the lines written in m.txt file
 inside the static folder'''
